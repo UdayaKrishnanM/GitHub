6db9c62a3437ed4b232d49e9189880d4fee68220
Udaya Krishnan.M <101326005+UdayaKrishnanM@users.noreply.github.com>
2024-02-23 14:33:50 +0530

    Add files via upload

---
+++ SpringfitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springfita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
